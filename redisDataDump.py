0aa1d945299e2255b914c247b843c7a15a3be099
reeveress <zuhraa470@gmail.com>
2017-10-09 16:36:00 -0700

    Added Karoo dataset for 100917

--- redisDataDump.py
+++ redisDataDump.py
@@ -1,7 +1,7 @@
 import os 
 import time
 while True:
-    os.system("redis-cli hgetall status:node:0 >> dataset100517.txt")
+    os.system("redis-cli hgetall status:node:0 >> dataset100917.txt")
     time.sleep(300)
 
     
--- serialDataDump.py
+++ serialDataDump.py
@@ -1,6 +1,6 @@
 import os 
 import time
 while True:
-    os.system("python udpSerial.py >> serial091917.txt")
+    os.system("python udpSerial.py >> serial100917.txt")
     time.sleep(100)
     
