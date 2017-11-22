c4ccc7a08b45ab84097aa9dabde2d290041c1847
reeveress <zuhraa470@gmail.com>
2017-11-22 10:43:03 -0800

    data collection scripts

--- redisDataDump.py
+++ redisDataDump.py
@@ -1,7 +1,7 @@
 import os 
 import time
 while True:
-    os.system("redis-cli hgetall status:node:0 >> dataset112017.txt")
+    os.system("redis-cli hgetall status:node:0 >> dataset112117.txt")
     time.sleep(300)
 
     
--- serialDataDump.py
+++ serialDataDump.py
@@ -1,6 +1,6 @@
 import os 
 import time
 while True:
-    os.system("python udpSerial.py >> serial112017.txt")
+    os.system("python udpSerial.py >> serial112117.txt")
     time.sleep(100)
     
